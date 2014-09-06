2794732fb436c22ad23df17ba69cd320d5b23434
asaulyak <jenya.asaulyak@gmail.com>
2014-09-07 00:22:20 +0300

    Play searced tracks

--- middleware/Channel.js
+++ middleware/Channel.js
@@ -8,20 +8,25 @@ var Channel = function () {
 	}
 
 	this.streamer = new Streamer();
+	this._tracks = [];
 
 	this.join = function (client) {
 		console.log('join client');
 		this.streamer.registerClient(client);
 	};
 
-	this.start = function (songId) {
-		console.log('start channel');
-//		this.streamer.play('');
+	this.start = function () {
+		console.log('start channel', this._tracks);
+		this.streamer.play(this._tracks.pop().url);
 	};
 
 	this.stop = function () {
 		this.streamer.stop();
 	};
+
+	this.addTrack = function (track) {
+		this._tracks.push(track);
+	}
 };
 
 module.exports = Channel;
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express'),
 	config = require('./config'),
 	Channel = require('./middleware/channel'),
 	async = require('async'),
-	mediaSources = require('./middleware/mediaSources');
+	mediaSources = require('./middleware/mediaSources'),
+	bodyParser = require('body-parser');
 
 var app = express(),
 	channels = {};
+
+app.use(bodyParser.json());
 
 app.get('/api/channel/play/:name', function (req, res) {
 	console.log('play channel', req.params.name, channels[req.params.name]);
@@ -20,9 +23,7 @@ app.get('/api/channel/play/:name', function (req, res) {
 
 app.get('/api/channel/create/:name', function (req, res) {
 	console.log('/channel/create/');
-	var channel = new Channel();
-	channels[req.params.name] = channel;
-	channel.start();
+	channels[req.params.name] = new Channel();
 
 	res.write('Channel ' + req.params.name + ' has been added.');
 	res.end();
@@ -34,10 +35,27 @@ app.get('/api/channel/remove/:name', function (req, res) {
 	res.end();
 });
 
+app.post('/api/channel/:name/addtrack/', function (req, res) {
+	var channel = channels[req.params.name];
+	if (channel) {
+
+		var engine = mediaSources.getEngine(req.body.engine);
+
+		engine.getTrack(req.body.id, function (err, track) {
+			if(!err) {
+				channel.addTrack(track);
+				channel.start();
+			}
+		});
+	}
+
+	res.end();
+});
+
 app.get('/api/search/:query', function (req, res) {
 
 	// Create array of tasks to be ran for each media source
-	var sources = mediaSources.map(function (source) {
+	var sources = mediaSources.getEngines().map(function (source) {
 		return function (callback) {
 			source.searchMusic(encodeURIComponent(req.params.query), callback);
 		}
